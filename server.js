40746e5780ba856019cfd3952740b4410bd5a855
Mark <mark.r.mahoney@gmail.com>
2017-04-02 15:09:03 +0000

    All Finished

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var unixTimestamp = require('unix-timestamp');
 
 var app = express();
+var dateObject = {};
+
+app.get('/:date',function(req,res,next){
+    if (!isNaN(unixTimestamp.fromDate(req.params.date))){
+        console.log(req.params.date)
+        dateObject.unix = unixTimestamp.fromDate(req.params.date);
+        dateObject.natural = (unixTimestamp.toDate(parseInt(unixTimestamp.fromDate(req.params.date)))).toString().substring(0,15);
+    }
+    
+    else if (unixTimestamp.toDate(parseInt(req.params.date)) != 'Invalid Date'){
+        console.log('was a date');
+        dateObject.unix = req.params.date;
+        dateObject.natural = (unixTimestamp.toDate(parseInt(req.params.date))).toString().substring(0,15);
+    }
+    else {
+        console.log('was neither')
+        dateObject.unix = null;
+        dateObject.natural = null;
+    }
+    next()
+})
 
 app.get('/', function(req,res){
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/:date', function(req,res){
+    res.send(JSON.stringify(dateObject))
 });
 
 app.listen(8080,function(){
