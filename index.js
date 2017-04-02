6bafe28e7c25faf9cecf0002990f248c1d549e37
Mark <mark.r.mahoney@gmail.com>
2017-04-02 16:32:56 +0000

    renamed server to index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var unixTimestamp = require('unix-timestamp');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
+
 var dateObject = {};
 
 app.get('/:date',function(req,res,next){
@@ -32,6 +34,6 @@ app.get('/:date', function(req,res){
     res.send(JSON.stringify(dateObject))
 });
 
-app.listen(8080,function(){
-    console.log('Listening on 8080!')
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
